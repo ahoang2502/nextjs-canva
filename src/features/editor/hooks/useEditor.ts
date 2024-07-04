5e23232c5e3c4b96199b9aa06197af42a78c47c0
ahoang2502 <athoang252@gmail.com>
2024-07-04 14:00:57 +1000

    feat: responsive canvas

--- src/features/editor/hooks/useEditor.ts
+++ src/features/editor/hooks/useEditor.ts
@@ -1,7 +1,17 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { fabric } from "fabric";
 
+import { useAutoResize } from "./useAutoResize";
+
 export const useEditor = () => {
+  const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  useAutoResize({
+    canvas,
+    container,
+  });
+
   const init = useCallback(
     ({
       initialCanvas,
@@ -38,6 +48,9 @@ export const useEditor = () => {
       initialCanvas.add(initialWorkspace);
       initialCanvas.centerObject(initialWorkspace);
       initialCanvas.clipPath = initialWorkspace;
+
+      setCanvas(initialCanvas);
+      setContainer(initialContainer);
 
       const test = new fabric.Rect({
         width: 100,
