a35254f7fc34ef74ad9d8b3fd8e3b40658fc734d
ahoang2502 <athoang252@gmail.com>
2024-07-05 14:21:12 +1000

    feat: editor layout

--- src/features/editor/components/Editor.tsx
+++ src/features/editor/components/Editor.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import { fabric } from "fabric";
+import { useEffect, useRef } from "react";
 
+import { Footer } from "@/features/editor/components/Footer";
+import { Navbar } from "@/features/editor/components/Navbar";
+import { Sidebar } from "@/features/editor/components/Sidebar";
+import { Toolbar } from "@/features/editor/components/Toolbar";
 import { useEditor } from "@/features/editor/hooks/useEditor";
 
 export const Editor = () => {
@@ -25,8 +29,23 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div ref={containerRef} className="flex-1 h-full bg-muted">
-        <canvas ref={canvasRef} />
+      <Navbar />
+
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar />
+
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+
+          <div
+            ref={containerRef}
+            className="flex-1 h-[calc(100%-124px)] bg-muted"
+          >
+            <canvas ref={canvasRef} />
+          </div>
+
+          <Footer />
+        </main>
       </div>
     </div>
   );
