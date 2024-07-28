d5cecdab7f64b883e17ff6960fdb72e144ed9654
ahoang2502 <athoang252@gmail.com>
2024-07-28 16:17:54 +1000

    feat: complete AI image generation

---
+++ src/features/ai/api/useGenerateImage.ts
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.ai)["generate-image"]["$post"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.ai)["generate-image"]["$post"]
+>["json"];
+
+export const useGenerativeImage = () => {
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.ai["generate-image"].$post({ json });
+
+      return await response.json();
+    },
+  });
+
+  return mutation;
+};
--- src/features/editor/components/AISidebar.tsx
+++ src/features/editor/components/AISidebar.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Textarea } from "@/components/ui/textarea";
+
+import { useGenerativeImage } from "@/features/ai/api/useGenerateImage";
 import { ToolSidebarClose } from "@/features/editor/components/ToolSidebarClose";
 import { ToolSidebarHeader } from "@/features/editor/components/ToolSidebarHeader";
 import { ActiveTool, Editor } from "@/features/editor/types";
 import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
 
 interface AISidebarProps {
   editor: Editor | undefined;
@@ -17,6 +21,20 @@ export const AISidebar = ({
   activeTool,
   onChangeActiveTool,
 }: AISidebarProps) => {
+  const mutation = useGenerativeImage();
+
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: Block with paywall
+
+    mutation.mutateAsync({ prompt: value }).then(({ data }) => {
+      editor?.addImage(data);
+    });
+  };
+
   const onClose = () => {
     onChangeActiveTool("select");
   };
@@ -31,16 +49,24 @@ export const AISidebar = ({
       <ToolSidebarHeader title="AI" description="Generate an image using AI" />
 
       <ScrollArea>
-        <form className="p-4 space-y-6">
+        <form onSubmit={onSubmit} className="p-4 space-y-6">
           <Textarea
+            disabled={mutation.isPending}
+            value={value}
             placeholder="An astronaut riding a horse on mars, hd, dramatic lighting"
             cols={30}
             rows={10}
             required
             minLength={3}
+            onChange={(e) => setValue(e.target.value)}
           />
 
-          <Button variant="primary" type="submit" className="w-full">
+          <Button
+            disabled={mutation.isPending}
+            variant="primary"
+            type="submit"
+            className="w-full"
+          >
             Generate
           </Button>
         </form>
