02f89befb5c6128548aa0bb7bf923d75d38a9250
ahoang2502 <athoang252@gmail.com>
2024-08-19 17:21:31 +1000

    feat: sign-up page +background image

---
+++ src/features/auth/components/SignUpCard.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { signIn } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const SignUpCard = () => {
+  const onProviderSignUp = (provider: "github" | "google") => {
+    signIn(provider, { callbackUrl: "/" });
+  };
+
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0 ">
+        <CardTitle>Create an account</CardTitle>
+
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+            onClick={() => onProviderSignUp("google")}
+          >
+            <FcGoogle className="mr-2 size-5 top-2.5 left-2.5 absolute" />
+            Continue with Google
+          </Button>
+
+          <Button
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+            onClick={() => onProviderSignUp("github")}
+          >
+            <FaGithub className="mr-2 size-5 top-2.5 left-2.5 absolute" />
+            Continue with Github
+          </Button>
+        </div>
+
+        <p className="text-xs text-muted-foreground text-center">
+          Already have an account?{" "}
+          <Link href="/sign-in">
+            <span className="text-sky-700 hover:underline">Sign in</span>
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
